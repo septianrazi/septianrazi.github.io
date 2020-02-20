c381dfb0f6ba0b9afc15c87ff3727044c2bdc3c7
Septian Razi <u6086829@anu.edu.au>
2020-02-21 01:14:07 +1100

    edited skills section to dashboard tab version

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -1,4 +1,6 @@
-// MASONRY
+// ----------
+// MASONRY 
+// ----------
 // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
 // init Masonry
@@ -15,6 +17,9 @@ imagesLoaded( grid ).on( 'progress', function() {
   msnry.layout();
 });
 
+// ----------
+// NAV SCROLL 
+// ----------
 // SOURCE: https://www.w3schools.com/howto/howto_js_sticky_navHeader.asp
 
 // When the user scrolls the page, execute stickyNavBar
@@ -34,9 +39,31 @@ function stickyNavBar() {
     } else {
         navHeader.classList.remove("sticky");
     }
-
-    console.log("executed");
 }
 
-console.log("yeet");
+// ----------
+// TABS 
+// ----------
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  document.getElementById("defaultOpen").click();
 
